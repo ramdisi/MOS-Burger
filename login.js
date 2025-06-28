ebc7ae0d00a7ff94294379ae25e55f3061e6203f
ramdisi <ramdisiabey@gmail.com>
2025-06-28 21:48:04 +0530

    login page backend done with js

---
+++ login.js
@@ -0,0 +1,23 @@
+function check() {
+    let role;
+    let isAuthentic = false;
+    let password = document.getElementById("password").value;
+    let user = document.getElementById("username").value;
+    let userArray = [{username:"ramdisi",password:"123",role:"admin"},]
+    userArray.forEach(element => {
+        if(element.username==user & element.password==password){
+            isAuthentic=true;
+            role = element.role
+        }
+    });
+    if(isAuthentic){
+        if (role=="admin") {
+            window.location.replace("admin.html");
+        } else {
+            window.location.replace("user.html");
+        }
+
+    }else{
+        document.getElementById("warning").innerHTML='<p style="color: red ;">Incorrect Password Or UserName</p>';
+    }
+}
